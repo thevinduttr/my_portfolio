5064409112e366d7fcd87252344b576bb971ba4d
Thevindu Rathnaweera <it22000880@my.sliit.lk>
2024-07-05 10:17:31 +0530

    aa

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 //next image
-import Image from 'next/image'
+import Image from "next/image"
 
 //components
 import ParticlesContainer from '../components/ParticlesContainer';
@@ -14,42 +14,42 @@ import { fadeIn } from '../variants';
 
 
 const Home = () => {
-  return <div className='bg-primary h-full'>
+  return <div className="bg-primary h-full">
     {/* text */}
-    <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
-      <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
+    <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+      <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
         {/* title */}
-        <motion.h1 variants={fadeIn('down' , 0.2)} initial='hidden' animate='show' exit='hidden' className='h1'>
-        Hello and Welcome! <br /> I'm{' '} <span className='text-accent'> Thevindu</span>
+        <motion.h1 variants={fadeIn('down' , 0.2)} initial="hidden" animate="show" exit="hidden" className="h1">
+        Hello and Welcome! <br /> I'm{' '} <span className="text-accent"> Thevindu</span>
         </motion.h1>
 
         {/* subtitle */}
-        <motion.p variants={fadeIn('down' , 0.3)} initial='hidden' animate='show' exit='hidden' className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-10'>
+        <motion.p variants={fadeIn('down' , 0.3)} initial="hidden" animate="show" exit="hidden" className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-10">
         I'm an aspiring software engineer currently pursuing my undergraduate studies. Passionate about coding and problem-solving, I've created this space to showcase my projects, skills, and experiences. 
         <br/><br/>Thank you for visiting, and enjoy exploring my work! 
         </motion.p>
 
         {/* btn */}
-        <div className='flex justify-center xl:hidden relative'>
+        <div className="flex justify-center xl:hidden relative">
           <ProjectsBtn />
         </div>
 
-        <motion.div variants={fadeIn('down' , 0.4)} initial='hidden' animate='show' exit='hidden' className='hidden xl:flex' >
+        <motion.div variants={fadeIn('down' , 0.4)} initial="hidden" animate="show" exit="hidden" className="hidden xl:flex" >
           <ProjectsBtn />
         </motion.div>
       </div>
     </div>
 
     {/* image */}
-    <div className='w-[1100px] h-full absolute right-0 bottom-0'>
+    <div className="w-[1100px] h-full absolute right-0 bottom-0">
       {/* bg image */}
-      <div className='bg-none xl:bg-explosion xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0'></div>
+      <div className="bg-none xl:bg-explosion xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
 
       {/* particles */}
       <ParticlesContainer />
 
       {/* avater img*/}
-      <motion.div variants={fadeIn('up' , 0.5)} initial='hidden' animate='show' exit='hidden' transition={{duration: 1 , ease: 'easeInOut'}} className='w-full h-full max-w-[637px] max-h-[578px] absolute -bottom-32 lg:bottom-0 lg:right-[1%]'>
+      <motion.div variants={fadeIn('up' , 0.5)} initial="hidden" animate="show" exit="hidden" transition={{duration: 1 , ease: 'easeInOut'}} className="w-full h-full max-w-[637px] max-h-[578px] absolute -bottom-32 lg:bottom-0 lg:right-[1%]">
         <Avatar />
       </motion.div>
     </div>
