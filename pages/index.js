db39b92861674c38917970a85329356393532950
Thevindu Rathnaweera <it22000880@my.sliit.lk>
2024-07-08 12:31:31 +0530

    fix errors

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ import { fadeIn } from "../variants";
 
 const Home = () => {
   return (
-    <div className="bg-primary h-full">
+    <div className="bg-primary h-full xl:pt-0 pt-10">
       {/* text */}
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
@@ -52,7 +52,7 @@ const Home = () => {
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="hidden xl:flex pt-0"
+            className="hidden xl:flex"
           >
             <ProjectsBtn />
           </motion.div>
@@ -60,7 +60,7 @@ const Home = () => {
       </div>
 
       {/* image */}
-      <div className="w-[1150px] xl:h-full absolute right-0 bottom-0">
+      <div className="w-[1100px] h-full absolute right-0 bottom-0">
         {/* bg image */}
         <div className="bg-none xl:bg-explosion xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
 
