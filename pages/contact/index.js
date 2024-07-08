6cd01a4df15eee9e615878d2eca242c5dd7c9f01
Thevindu Rathnaweera <it22000880@my.sliit.lk>
2024-07-08 09:28:02 +0530

    arrange pt for home page

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -13,7 +13,7 @@ import { fadeIn } from '../../variants'
 const Contact = () => {
   return (
     <div className="h-full bg-primary">
-      <div className="container mx-auto  text-center xl:text-left flex items-center justify-center h-full">
+      <div className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
         {/* text & form */}
         <div className="flex flex-col w-full max-w-[700px]">
           {/* text */}
